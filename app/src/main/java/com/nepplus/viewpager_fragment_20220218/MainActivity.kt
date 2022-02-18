926185e9c0ba6d053e779da38106d9a2ca814a27
이 영 종 <naki5087@naver.com>
2022-02-18 17:10:25 +0900

    한번에 여러 페이지를 들고 있도록 세팅

--- app/src/main/java/com/nepplus/viewpager_fragment_20220218/MainActivity.kt
+++ app/src/main/java/com/nepplus/viewpager_fragment_20220218/MainActivity.kt
@@ -20,6 +20,9 @@ class MainActivity : AppCompatActivity() {
     fun setValues(){
         mAdapter = MainViewPaperAdapter(supportFragmentManager)
         mainViewPager.adapter = mAdapter
+
+//        재사용 성 무시하도록 하는 코드 예시 (사용성 개선)
+        mainViewPager.offscreenPageLimit = 5
     }
 
 }
