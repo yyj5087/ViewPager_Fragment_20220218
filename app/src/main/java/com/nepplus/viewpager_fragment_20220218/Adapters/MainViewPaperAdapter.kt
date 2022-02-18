9cee24cf193b4a8599943b9840914858bb279b13
이 영 종 <naki5087@naver.com>
2022-02-18 17:01:28 +0900

    필수 구현 함수 2개 완성  - 총 몇장짜리인지  - 각 position에 어떤프래그먼트가 나가는지

--- app/src/main/java/com/nepplus/viewpager_fragment_20220218/Adapters/MainViewPaperAdapter.kt
+++ app/src/main/java/com/nepplus/viewpager_fragment_20220218/Adapters/MainViewPaperAdapter.kt
@@ -1,8 +1,27 @@
 package com.nepplus.viewpager_fragment_20220218.Adapters
 
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.nepplus.viewpager_fragment_20220218.Fragments.BirthYearFragment
+import com.nepplus.viewpager_fragment_20220218.Fragments.HelloFragment
+import com.nepplus.viewpager_fragment_20220218.Fragments.NameFragment
 
 class MainViewPaperAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getItem(position: Int): Fragment {
+        if(position == 0){
+            return NameFragment()
+        }
+        else if (position == 1){
+            return BirthYearFragment()
+        }
+        else{
+            return HelloFragment()
+        }
+    }
 
 }
